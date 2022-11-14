6721c866c742658a5f439bad050dbe742370141c
Cécile Kremer <30626850+cecilekremer@users.noreply.github.com>
2022-11-14 19:00:20 +0100

    export summary function

--- R/fun_summData.R
+++ R/fun_summData.R
@@ -1,12 +1,21 @@
-
-summarize.indiv.data <- function(data, # in order of: dose, response, covariate OR dose, response, litter
+#' Function to summarize individual data
+#'
+#' @param data data in order of: dose, response, covariate OR dose, response, litter
+#' @param type either 'continuous' or 'quantal'
+#' @param cluster logical indicating whether the data include a litter effect (defaults to FALSE)
+#' @param covar logical indicating whether the data include a covariate (defaults to FALSE)
+#'
+#' @return dataframe of summary data
+#'
+#' @export summarize.indiv.data
+summarize.indiv.data <- function(data,
                                  type = c('continuous','quantal'), cluster = FALSE, covar = FALSE){
   require('dplyr')
-  
+
   if(type == 'continuous'){
-    
+
     if(cluster == FALSE && covar == FALSE){
-      
+
       data = data[order(data[, 1]), ]
       doses = data[, 1]
       maxDose = max(doses)
@@ -22,15 +31,15 @@ summarize.indiv.data <- function(data, # in order of: dose, response, covariate 
         n.a[iu]=sum(doses==dose.a[iu])
       }
       dose.a = dose.a/maxDose
-      
+
       summary.data <- data.frame(Dose = dose.a*maxDose,
                                  Response = mean.a,
                                  SD = sd.a,
                                  N = n.a)
-      
-      
+
+
     }else if(cluster == FALSE && covar == TRUE){
-      
+
       data = data[order(data[, 1]), ]
       data$dose = data[,1]
       data$resp = data[,2]
@@ -46,16 +55,16 @@ summarize.indiv.data <- function(data, # in order of: dose, response, covariate 
       n.a = indiv.data$n
       N = length(dose.a)
       dose.a = dose.a/maxDose
-      covar = indiv.data$cov    
-      
+      covar = indiv.data$cov
+
       summary.data <- data.frame(Dose = dose.a*maxDose,
                                  Response = mean.a,
                                  SD = sd.a,
                                  N = n.a,
                                  Covariate = covar)
-      
+
     }else if(cluster == TRUE && covar == FALSE){
-      
+
       library(dplyr)
       indiv.data <- data.frame(dose = data[,1],
                                response = data[,2],
@@ -67,25 +76,25 @@ summarize.indiv.data <- function(data, # in order of: dose, response, covariate 
         dplyr::summarise(mean.a = mean(response), sd.a = sd(response), n.a = n())
       # indiv.data <- indiv.data %>%
       #   dplyr::summarise()
-      #   
+      #
       #   dplyr::mutate(cluster = dplyr::cur_group_id(),
       #                 count = n())
-      
-      
+
+
       summary.data <- data.frame(Dose = indiv.data$dose,
                                     Response = indiv.data$mean.a,
                                     SD = indiv.data$sd.a,
                                     N = indiv.data$n.a)
-      
-      
+
+
     }else if(cluster == TRUE && covar == TRUE){
       stop('Covariates not implemented for clustered data')
     }
-    
+
   }else if(type == 'quantal'){
-    
+
     if(cluster == FALSE && covar == FALSE){
-      
+
       doses = data[, 1]
       dose.a = unique(doses)
       N = length(dose.a)
@@ -99,13 +108,13 @@ summarize.indiv.data <- function(data, # in order of: dose, response, covariate 
         n.a[iu] = sum(nbin[doses==dose.a[iu]])
       }
       dose.a = dose.a/maxDose
-      
+
       summary.data <- data.frame(Dose = dose.a*maxDose,
                                  n.Events = y.a,
                                  N = n.a)
-      
+
     }else if(cluster == FALSE && covar == TRUE){
-      
+
       data = data[order(data[, 1]), ]
       data$dose = data[,1]
       data$ybin = data[,2]
@@ -120,16 +129,16 @@ summarize.indiv.data <- function(data, # in order of: dose, response, covariate 
       n.a = indiv.data$n
       N = length(dose.a)
       dose.a = dose.a/maxDose
-      covar = indiv.data$cov    
+      covar = indiv.data$cov
       dose.a = dose.a/maxDose
-      
+
       summary.data <- data.frame(Dose = dose.a*maxDose,
                                  n.Events = y.a,
                                  N = n.a,
                                  Covariate = covar)
-      
+
     }else if(cluster == TRUE && covar == FALSE){
-      
+
       doses = data[, 1]
       dose.a = unique(doses)
       maxDose = max(dose.a)
@@ -154,18 +163,18 @@ summarize.indiv.data <- function(data, # in order of: dose, response, covariate 
         n.a[iu] = sum(nbin[doses==dose.a[iu]])
       }
       dose.a = dose.a/maxDose
-      
+
       summary.data <- data.frame(Dose = dose.a*maxDose,
                                  n.Events = y.a,
                                  N = n.a)
-      
-      
+
+
     }else if(cluster == TRUE && covar == TRUE){
       stop('Covariates not implemented for clustered data')
     }
-    
+
   }
-  
+
   return(summary.data)
-  
+
 }
